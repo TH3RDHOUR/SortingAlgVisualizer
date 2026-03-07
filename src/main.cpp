9581d3eb278830f0012418812dde59945049503e
TH3RDHOUR <daltocarter@outlook.com>
2026-03-07 14:09:34 -0600

    Basic main.cpp setup for drawing 1 Rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,27 @@
 
 int main()
 {
+    // Create the window.
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Sorting Visualizer");
 
     while (window.isOpen())
     {
+        // Check Window events triggered since last iteration of the loop.
         while (auto event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
 
-        window.clear();
+        // Clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // Create Rectangle .
+        sf::RectangleShape rectangle({50.0f, 120.0f});
+
+        window.draw(rectangle);
+
+        // Take what is drawn and show on the Window.
         window.display();
     }
 }
