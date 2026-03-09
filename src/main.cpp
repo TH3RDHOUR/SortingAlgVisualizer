08131d8637b6f7025296f407bef356bcb8652d88
TH3RDHOUR <daltocarter@outlook.com>
2026-03-09 14:23:09 -0500

    Set window resize handler, rectangles now resize with window.

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,14 @@ int main()
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
+
+            // Handle the window being resized.
+            if (auto* resized = event->getIf<sf::Event::Resized>())
+            {
+                sf::FloatRect visibleArea({0.f, 0.f}, 
+                    {static_cast<float>(resized->size.x), static_cast<float>(resized->size.y)});
+                window.setView(sf::View(visibleArea));
+            }
         }
 
         drawVector(arr, window);
@@ -40,14 +48,18 @@ void drawVector(const std::vector<int>& arr, sf::RenderWindow& window)
     window.clear();
     for (int i = 0; i < arr.size(); ++i)
     {
-        sf::RectangleShape rectangle(sf::Vector2f(20, arr[i]));
-        rectangle.setSize(sf::Vector2f(20, arr[i]));
-
         // Get window height for rectangles to be on bottom.
         float windowHeight = window.getSize().y;
+        float windowWidth = window.getSize().x;
+        float rectWidth = windowWidth / arr.size();
+
+        sf::RectangleShape rectangle(sf::Vector2f(20, arr[i]));
+        rectangle.setSize(sf::Vector2f(rectWidth - 5.f, arr[i]));
+
         // Space out rectangles & set height based on bottom of the screen size.
-        rectangle.setPosition(sf::Vector2f(i * 25, windowHeight - rectangle.getSize().y));
+        rectangle.setPosition(sf::Vector2f(i * rectWidth, windowHeight - arr[i]));
         rectangle.setFillColor(sf::Color::White);
+
         window.draw(rectangle);
     }
     window.display();
